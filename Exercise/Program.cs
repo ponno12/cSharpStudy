cecd69d5780844fdcdf18a070475a4bac4043268
ponno12 <ponno145@gmail.com>
2021-08-03 22:46:49 +0900

    stack quqe, list study

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercise
 {
@@ -6,7 +7,23 @@ namespace Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> stack = new Stack<int>();
+
+
+
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(101);
+            queue.Enqueue(102);
+            queue.Enqueue(103);
+            queue.Enqueue(104);
+            queue.Enqueue(105);
+            queue.Enqueue(106);
+            queue.Enqueue(107);
+
+            int data3 = queue.Dequeue();
+            int data4 = queue.Peek();
         }
+
     }
 }
