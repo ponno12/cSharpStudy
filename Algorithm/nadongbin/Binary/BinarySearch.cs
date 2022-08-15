39be8218b4c7afd089ab2b0b8208c36351f11133
Ponno12 <ponno145@gmail.com>
2022-08-15 23:12:52 +0900

    BinarySearch

--- Algorithm/nadongbin/Binary/BinarySearch.cs
+++ Algorithm/nadongbin/Binary/BinarySearch.cs
@@ -4,23 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise
+namespace Algorithm.nadongbin.Binary
 {
-    /// <summary>
-    /// 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
-    /// </summary>
-    public class Solution
+    public class BinarySearch
     {
-
-        public static void Main()
+        public void Main()
         {
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int answer = (int)MyBinarySearch(array, 9, 0,9);
+            int answer = (int)BinarySearchIterative(array, 9);
             Console.WriteLine(answer);
         }
 
-        
-            //반복문으로 하는버전
+        //반복문으로 하는버전
         public static object BinarySearchIterative(int[] inputArray, int key)
         {
             int min = 0;
@@ -67,29 +62,5 @@ namespace Exercise
                 }
             }
         }
-        
-        public static object MyBinarySearch(int[] array, int key, int min, int max)
-        {
-            if (min > max)
-                return 1000;
-            else
-            {
-                int mid = (min + max) / 2;
-                if (array[mid] == key)
-                {
-                    return ++mid;
-                }
-                else if (key < array[mid])
-                {
-                    return MyBinarySearch(array, key, min, mid - 1);
-                }
-                else
-                {
-                    return MyBinarySearch(array, key, mid + 1, max);
-                }
-            }
-            
-        }
-
     }
 }
